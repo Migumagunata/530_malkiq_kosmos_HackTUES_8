14cd1597f10c4749edd873c13135ff4bdb6edc16
mihail.boshkilov <86418524+Migumagunata@users.noreply.github.com>
2022-03-11 17:50:11 +0200

    код за пускане на таймер
    Когато сензора за дим и газ засече наличието им, се изписва FIRE на OLED екаран и почва да брой от 60 до 0

--- sensordetection.ino
+++ sensordetection.ino
@@ -5,19 +5,46 @@
 #define screen_width 128
 #define screen_height 32
 
+#define smokeA0 A2
 
-int sensorThres = 100;
-int smokeA0 = A2;
+int sensorThres = 400;
 
 
 Adafruit_SSD1306 display(screen_width, screen_height);
 
+void fire()
+{
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.print("FIRE");
+  display.display();
+}
+int iCountdown()
+{
+  int countdown=60;
+  while (countdown !=0)
+  {
+    
+    display.clearDisplay();
+    display.setTextSize(2);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(0, 16);
+    display.print(countdown);
+    display.display();
+    fire();
+    delay(1000);
+    countdown--;
+  }
+    return 0; 
+}
 
 void setup()
 {
+  Serial.begin(9600);
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   pinMode(smokeA0, INPUT);
-  Serial.begin(9600);
 }
  
  
@@ -26,14 +53,6 @@ void loop()
 int analogSensor = analogRead(smokeA0);
  if (analogSensor > sensorThres)
   {
-    display.clearDisplay();
-    display.setTextSize(2);
-    display.setTextColor(SSD1306_WHITE);
-    display.setCursor(0, 0);
-    display.print("FIRE");
-    
-  }
-  
-
-  display.display();
+    iCountdown(); 
+  }  
 }
